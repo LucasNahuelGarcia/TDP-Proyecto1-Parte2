0aa5f63fd1937348a44801788c2f02a7de0ffdd7
Lucas Garcia <nahuellucasgarcia@gmail.com>
2020-09-19 16:14:33 -0300

    agregué la definición de los metodos pedidos por el enunciado

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -1,5 +1,44 @@
 package Graph;
 
 public class Graph {
-  
+  /**
+   * Agrega el nodo "node" al grafo, si aún no pertenecía a la estructura.
+   * 
+   * @param node nodo a agregar.
+   */
+  public void addNode(int node) {
+
+  }
+
+  /**
+   * Agrega un arco entre el nodo “node1” y el nodo “node2”, si aún no existía el
+   * arco y ambos parámetros son nodos pertenecientes a la estructura.
+   * 
+   * @param node1 nodo que tiene como extremo el arco.
+   * @param node2 nodo que tiene como extremo el arco.
+   */
+  public void addEdge(int node1, int node2) {
+
+  }
+
+  /**
+   * Remueve el nodo "node" del grafo, si el parámetro es un nodo de la
+   * estructura.
+   * 
+   * @param node nodo a eliminar.
+   */
+  public void removeNode(int node) {
+
+  }
+
+  /**
+   * Remueve el arco entre el nodo "node1" y el nodo "node2", si el arco que los
+   * conecta pertenece a la estructura.
+   * 
+   * @param node1
+   * @param node2
+   */
+  public void removeEdge(int node1, int node2) {
+
+  }
 }
