7ac8613067310920bd826c65708dbc703f053f1b
Lucas Garcia <nahuellucasgarcia@gmail.com>
2020-09-21 00:08:34 -0300

    agegue casos de prueba para eliminar nodos y arcos

--- Graph/LoggingPrueba.java
+++ Graph/LoggingPrueba.java
@@ -3,27 +3,36 @@ package Graph;
 public class LoggingPrueba {
   public static void main(String args[]) {
     Graph grafo = new Graph();
-    
-    //Agrego nodos
+
+    // Agrego nodos y arcos
+    grafo.addNode(2);
+    grafo.addNode(4);
+    grafo.addEdge(2, 4);
+    grafo.addEdge(4, 2);
+
+    // Agrego nodos y arcos repetidos
     grafo.addNode(2);
     grafo.addNode(4);
     grafo.addEdge(2, 4);
 
-    //Agrego nodos y arcos repetidos
-    grafo.addNode(2);
-    grafo.addNode(4);
-    grafo.addEdge(2,4);
-
-    //Agrego arcos con nodos que no existen
+    // Agrego arcos con nodos que no existen
     grafo.addEdge(2, 8);
     grafo.addEdge(9, 4);
     grafo.addEdge(5, 23);
 
-    //elimino arco existente
+    // Elimino arco existente
+    grafo.removeEdge(2, 4);
+    grafo.removeEdge(4, 2);
+
+    // Lo elimino de vuelta
     grafo.removeEdge(2, 4);
 
-    //lo elimino de vuelta
-    grafo.removeEdge(2, 4);
-  
+    // Elimino nodos
+    grafo.removeNode(2);
+    grafo.removeNode(4);
+
+    // Elimino nodos no existentes
+    grafo.removeNode(2);
+    grafo.removeNode(211);
   }
 }
